567271074374a1e0a15ab6d41bb93b15f7e1ad25
Solln <Darren-Lowe@live.co.uk>
2021-06-12 13:57:51 +0100

    Initial commit

---
+++ src/main/java/com/pog/pepeflights/PepeflightsApplication.java
@@ -0,0 +1,13 @@
+package com.pog.pepeflights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PepeflightsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PepeflightsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pog/pepeflights/PepeflightsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pog.pepeflights;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PepeflightsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
